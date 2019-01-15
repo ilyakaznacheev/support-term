5c243364546170751d2fbf99904a7626593271a7
Ilya Kaznacheev <dreamworm@ya.ru>
2019-01-15 17:23:24 +0300

    Refactor client

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,19 +1,93 @@
 package client
 
 import (
-	"runtime"
-	"strings"
-	"time"
-
-	// "log"
 	"bufio"
+	"context"
 	"fmt"
+	"io"
 	"os"
 	"os/signal"
+	"strings"
+	"time"
 
 	"github.com/ilyakaznacheev/support-term/internal/types"
 	nats "github.com/nats-io/go-nats"
 )
+
+type client struct {
+	name   string
+	ec     *nats.EncodedConn
+	in     io.Reader
+	out    io.Writer
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+// NewClient create client app
+func newClient(nc *nats.Conn, name string, in io.Reader, out io.Writer) *client {
+	ec, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
+	ctx, cancel := context.WithCancel(context.Background())
+	c := &client{
+		name:   name,
+		ec:     ec,
+		in:     in,
+		out:    out,
+		ctx:    ctx,
+		cancel: cancel,
+	}
+	c.handleInterrupt()
+
+	return c
+}
+
+func (c *client) handleInterrupt() {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	go func() {
+		<-interrupt
+		c.cancel()
+	}()
+}
+
+func (c *client) runMessageLoop() error {
+	reader := bufio.NewReader(c.in)
+
+	for {
+
+		fmt.Fprintln(c.out, "Enter your question")
+
+		question, _ := reader.ReadString('\n')
+		msg := &types.Question{
+			ID:       c.requestID(),
+			UserName: c.name,
+			Text:     strings.TrimSpace(question),
+		}
+
+		resp := &types.Answer{}
+		err := c.ec.Request("question", msg, resp, time.Minute)
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(c.out, "%s: %s\n", resp.SupName, resp.Text)
+
+		if c.ctx.Err() != nil {
+			break
+		}
+	}
+
+	fmt.Fprintln(c.out, "Good luck, "+c.name)
+	return nil
+}
+
+func (c *client) requestID() int64 {
+	resp := &types.NextID{}
+	err := c.ec.Request("id-request", nil, resp, time.Minute)
+	if err != nil {
+		return 0
+	}
+
+	return resp.GeneratedID
+}
 
 // Run start client support app
 func Run() error {
@@ -21,50 +95,12 @@ func Run() error {
 	if err != nil {
 		return err
 	}
-	ec, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
-	defer ec.Close()
-
-	reader := bufio.NewReader(os.Stdin)
+	defer nc.Close()
 
 	fmt.Println("Enter your name")
-	name, _ := reader.ReadString('\n')
-	name = strings.TrimSpace(name)
+	name, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-	go func() {
-		<-interrupt
-		ec.Close()
-		fmt.Println("Good luck, " + name)
-		runtime.Goexit()
-	}()
+	cl := newClient(nc, strings.TrimSpace(name), os.Stdin, os.Stdout)
 
-	for {
-		fmt.Println("Enter your question")
-
-		question, _ := reader.ReadString('\n')
-		msg := &types.Question{
-			ID:       RequestID(ec),
-			UserName: name,
-			Text:     strings.TrimSpace(question),
-		}
-
-		resp := &types.Answer{}
-		err := ec.Request("question", msg, resp, time.Minute)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("%s: %s\n", resp.SupName, resp.Text)
-	}
-}
-
-// RequestID sent request for a new ID
-func RequestID(ec *nats.EncodedConn) int64 {
-	resp := &types.NextID{}
-	err := ec.Request("id-request", nil, resp, time.Minute)
-	if err != nil {
-		return 0
-	}
-
-	return resp.GeneratedID
+	return cl.runMessageLoop()
 }
